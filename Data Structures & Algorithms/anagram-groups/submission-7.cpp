c60387d19220b7871596a77d4c82c0e9f9f96e58
AO5002 <154623452+AO-5002@users.noreply.github.com>
2026-08-02 14:55:16 -0500

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    string encode(string s){
+        vector<int> code(26, 0);
+        for(char c : s) code[c - 'a']++;
+
+        string encoding;
+        for(int i : code) encoding += to_string(i) + ".";
+        return encoding;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        unordered_map<string, vector<string>> mp;
+
+        for(string s : strs){
+            mp[encode(s)].push_back(s);
+        }
+
+        vector<vector<string>> res;
+        for(auto [_, v] : mp){
+            res.push_back(v);
+        }
+
+        return res;
+    }
+};
