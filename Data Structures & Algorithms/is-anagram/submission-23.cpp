be0b91e9f03f37eeac1a46a18a7f3cb041455d47
AO5002 <154623452+AO-5002@users.noreply.github.com>
2026-08-02 14:27:05 -0500

    Add: is-anagram - submission-23

---
+++ Data Structures & Algorithms/is-anagram/submission-23.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> s_mp;
+        unordered_map<char, int> t_mp;
+
+        for(char c : s){
+            s_mp[c]++;
+        }
+
+        for(char c : t){
+            t_mp[c]++;
+        }
+
+        return s_mp == t_mp;
+    }
+};
