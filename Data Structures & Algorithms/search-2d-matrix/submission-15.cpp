896831a66316e1f05adcabd473d8ba79b46d16ec
AO5002 <154623452+AO-5002@users.noreply.github.com>
2026-08-06 16:21:28 -0500

    Add: search-2d-matrix - submission-15

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-15.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& m, int target) {
+        int t = 0, b = m.size() - 1;
+        int ans_row = -1;
+
+        while (t <= b) {
+            int mid = (t + b) / 2;
+            if (target >= m[mid][0] && target <= m[mid][m[mid].size() - 1]) {
+                ans_row = mid;
+                return binary_search(m[ans_row].begin(), m[ans_row].end(), target);
+            }
+            
+            if (m[mid][0] > target) b = mid - 1;
+            else t = mid + 1;
+        }
+
+        return false;
+    }
+};
