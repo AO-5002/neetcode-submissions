47dbdea7076243f14546614766bb784d88acb96d
AO5002 <154623452+AO-5002@users.noreply.github.com>
2026-08-13 10:14:07 -0500

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& p, int h) {
+        int l = 1, r = *max_element(p.begin(), p.end()), k = r;
+
+        while(l <= r){
+
+            int m = (l + r) / 2, count = 0;
+            for(int i : p) count += (ceil((double)i / m));
+            if(count <= h) {
+                k = m;
+                r = m - 1;
+            }
+            else l = m + 1;
+        }
+
+        return k;
+    }
+};
